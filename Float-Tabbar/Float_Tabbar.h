d8b407fd86a034810fa588d6a12d055f6ff12b74
MauroJuliano <43898163+MauroJuliano@users.noreply.github.com>
2021-07-13 16:24:40 -0400

    Initial Commit

---
+++ Float-Tabbar/Float_Tabbar.h
@@ -0,0 +1,18 @@
+//
+//  Float_Tabbar.h
+//  Float-Tabbar
+//
+//  Created by user195713 on 7/13/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Float_Tabbar.
+FOUNDATION_EXPORT double Float_TabbarVersionNumber;
+
+//! Project version string for Float_Tabbar.
+FOUNDATION_EXPORT const unsigned char Float_TabbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Float_Tabbar/PublicHeader.h>
+
+
